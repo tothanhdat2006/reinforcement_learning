update: fix calculation and plot

10armed_testbed/main.py
@@ -43,7 +43,6 @@ def run_simulation(k, eps):
             num_opt_action[step_idx-1] = num_opt_action[step_idx-2] + (action == np.argmax(q_true)) # for fig 2.3
         percent_opt_action[step_idx-1] = num_opt_action[step_idx-1] / step_idx # for fig 2.3
 
-    reward_per_steps /= num_steps
     return reward_per_steps, percent_opt_action
 
 if __name__ == "__main__":
@@ -54,27 +53,30 @@ if __name__ == "__main__":
     percent_opt_action_list = np.array([[0.0]*1000 for _ in range(len(eps_list))])
     avg_reward_list = np.array([[0.0]*1000 for _ in range(len(eps_list))])
 
-    plt.figure(figsize=(16, 8))
+    plt.figure(figsize=(12, 8))
     for eps_idx, eps in enumerate(eps_list):
-        print("Running simulation for eps=0.0")
+        print(f"Running simulation for eps={eps}")
         for run_idx in tqdm(range(1, num_runs+1)):
             reward_per_steps, percent_opt_action = run_simulation(k, eps)
             avg_reward_list[eps_idx] += (reward_per_steps - avg_reward_list[eps_idx]) / run_idx # avg over runs
             percent_opt_action_list[eps_idx] += (percent_opt_action - percent_opt_action_list[eps_idx]) / run_idx # avg over runs
-        plt.plot(avg_reward_list[eps_idx], color=colors[eps_idx])
+        plt.plot(avg_reward_list[eps_idx], color=colors[eps_idx], label=f"eps={eps}")
     plt.xlabel("Steps")
     plt.ylabel("Average reward")
+    plt.hlines(y=0, xmin=0, xmax=1000, color='k', linestyle='--')
     plt.xlim(0, 1000)
     plt.ylim(0, 1.5)
+    plt.legend()
     plt.savefig("./outputs/fig22.jpg")
     # plt.show()
 
-    plt.figure(figsize=(16, 8))
+    plt.figure(figsize=(12, 8))
     for eps_idx in range(len(eps_list)):
-        plt.plot(percent_opt_action_list[eps_idx], color=colors[eps_idx])
+        plt.plot(percent_opt_action_list[eps_idx], color=colors[eps_idx], label=f"eps={eps_list[eps_idx]}")
     plt.xlabel("Steps")
     plt.ylabel("% Optimal Action")
     plt.xlim(0, 1000)
     plt.ylim(0, 1)
+    plt.legend()
     plt.savefig("./outputs/fig23.jpg")
     # plt.show()